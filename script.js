513133ff110966d8c4289009f94b6246fc62a02b
ansonh4294 <73484622+ansonh4294@users.noreply.github.com>
2020-10-26 08:05:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Anson H"
+let userName = "Ansonh4294";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
